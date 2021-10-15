cffbc552f30c0a16a14827469ebd143334bf5791
TheHeroCraft1579 <maximilian.doetsch@outlook.de>
2021-10-15 19:36:09 +0200

    Added info command, queue and searching

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { Client, Intents, Collection, VoiceChannel } = require('discord.js');
+const { Client, Intents, Collection, VoiceChannel, MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
 
 const { generateDependencyReport, joinVoiceChannel, getVoiceConnection, createAudioPlayer, createAudioResource, VoiceConnection, VoiceConnectionStatus } = require('@discordjs/voice');
 console.log(generateDependencyReport());
@@ -15,37 +16,101 @@ const client = new Client({ intents: ['GUILD_VOICE_STATES', 'GUILD_MESSAGES', 'G
 var queue = [];
 var connection = {};
 var playing = false;
-var connected = false;
 var connection = {};
+var channel = {};
+
+function play(channel, url) {
+    const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+
+    this.connection = getVoiceConnection(channel.guild.id);
+    const stream = ytdl(url, {filter: 'audioonly'});
+    const player = createAudioPlayer();
+    var resource = createAudioResource(stream);
+    player.play(resource);
+    this.connection.subscribe(player);
+}
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
     if (interaction.commandName == "play") {
-        const channel = interaction.member.voice.channel;
-        const url = interaction.options.getString("url");
+        channel = interaction.member.voice.channel;
 
-        if (!this.connected) {
-            const connection = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            });
+        if (!channel) {
+            await interaction.reply("❎ You need to be in a voice channel!");
+            return;
+        }
 
-            this.connected = true;
+        interaction.reply("Please wait");
+
+        const arg = interaction.options.getString("url");
+        var url = "";
+
+        if (!arg.includes("youtube.com")) {
+            const results = await ytSearch.search(arg);
+            url = results.all[0].url;
+        } else {
+            url = arg;
         }
 
         const info = await ytdl.getInfo(url);
 
-        console.log(`Playing ${url}`);
-        interaction.reply(`Playing ${info.videoDetails.title}`);
+        queue.push(url);
+        if (queue.length > 1) {
+            await interaction.editReply(`Added **${info.videoDetails.title}** to queue`);
+        } else {
+            await interaction.editReply(`Playing **${info.videoDetails.title}**`);
+            play(channel, queue[0]);
+        }
 
-        this.connection = getVoiceConnection(interaction.guild.id);
-        const stream = ytdl(url, {filter: 'audioonly'});
-        const player = createAudioPlayer();
-        var resource = createAudioResource(stream);
-        player.play(resource);
-        this.connection.subscribe(player);
+        const embed = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Playing")
+            .setURL(url)
+            .setThumbnail(info.videoDetails.thumbnails[0].url)
+            .addField("Artist", info.videoDetails.ownerChannelName)
+            .addField("Title", info.videoDetails.title)
+            .addField("Duration", info.videoDetails.lengthSeconds + "s");
+
+        interaction.followUp({embeds: [embed]});
+    }
+
+    if (interaction.commandName == "next") {
+        if (!queue[1]) {
+            interaction.reply("This was the last song!");
+            queue.shift();
+            return;
+        }
+
+        queue.shift();
+        play(channel, queue[0]);
+    }
+
+    if (interaction.commandName == "info") {
+        const url = queue[0];
+        if (!url) {
+            interaction.reply("I'm not playing anything... Sorry!");
+            return;
+        }
+
+        interaction.reply("Please wait");
+
+        const info =  ytdl.getInfo(url);
+
+        const embed = new MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle("Playing")
+            .setURL(url)
+            .setThumbnail(info.videoDetails.thumbnails[0].url)
+            .addField("Artist", info.videoDetails.ownerChannelName)
+            .addField("Title", info.videoDetails.title)
+            .addField("Duration", info.videoDetails.lengthSeconds + "s");
+
+        interaction.followUp({embeds: [embed]});
     }
 });
 
--- register-commands.js
+++ register-commands.js
@@ -1,23 +1,34 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { token, clientId } = require('./config.json');
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
 
 const commands = [
-	new SlashCommandBuilder()
-		.setName('play')
-		.setDescription('Plays a youtube video or audio streams')
-		.addStringOption(option => {
-			return option
-				.setName("url")
-				.setDescription("The video url")
-				.setRequired(true);
-		})
-	]
-	.map(command => command.toJSON());
+	{
+		name: "play",
+		description: "Plays a youtube video",
+		options:[
+			{
+				type: 3,
+				name: "url",
+				description: "The video url",
+				required: true
+			}
+		]
+	},
+	{
+		name: "next",
+		description: "Plays the next song in the queue"
+	},
+	{
+		name: "info",
+		description: "Returns info about the current song"
+	}
+]
 
 const rest = new REST({ version: '9' }).setToken(token);
-
 rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
